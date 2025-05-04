922d5ef7f31bb2a531395a0cadbed5b76308b4a9
Raffi Fabiansyah <133511291+Raffi14@users.noreply.github.com>
2025-05-04 14:18:47 +0700

    Update envConfig.ts

--- src/envConfig.ts
+++ src/envConfig.ts
@@ -1,6 +1,6 @@
 export const defaultConfig = {
   databaseUrl: process.env.DATABASE_URL,
-  databaseMaxPool: 200,
+  databaseMaxPool: 40,
   secretKey: process.env.SECRET_KEY,
   webUrl: process.env.NEXT_PUBLIC_API_URL,
 } as const;
